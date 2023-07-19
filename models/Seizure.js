628606d37bb6b118a6956fae11c505abffde03a5
DKCryptoKing <peter@paaskegram.dk>
2023-07-19 14:44:05 +0200

    seizure overview completed

--- models/Seizure.js
+++ models/Seizure.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 
 const seizureSchema = new mongoose.Schema({
   date: { type: Date, required: true },
-  location: { type: String, required: true },
-  severity: { type: String, required: true },
-  duration: { type: String, required: true },
-  vomiting: { type: String, required: true },
-  calledHome: { type: String, required: true },
-  trigger: { type: String, required: true },
+  location: { type: String, required: true, enum: ['School', 'Home', 'Other'] },
+  severity: { type: String, required: true, enum: ['Mild', 'Moderate', 'Severe'] },
+  duration: { type: String, required: true, enum: ['Few hours', 'Half day', 'Full day'] },
+  vomiting: { type: String, required: true, enum: ['Yes', 'No'] },
+  calledHome: { type: String, required: true, enum: ['Yes', 'No', 'N/A'] },
+  trigger: { type: String, required: true, enum: ['Unknown', 'Stress', 'Chock', 'Illness', 'Spontaneous'] },
   note: { type: String },
 });
+
 
 const Seizure = mongoose.model('Seizure', seizureSchema);
 
--- public/js/dbData.js
+++ public/js/dbData.js
@@ -1,69 +1,11 @@
-// Function to handle editing a record
-function editRecord(recordId) {
-  // Fetch the record data from the API
-  fetch(`/api/data/${recordId}`)
-    .then((response) => response.json())
-    .then((record) => {
-      // Populate the form fields with the record data
-      document.getElementById('editModalLabel').textContent = 'Edit Record';
-      document.getElementById('location').value = record.location;
-      document.getElementById('severity').value = record.severity;
-      document.getElementById('duration').value = record.duration;
-      document.getElementById('vomiting').value = record.vomiting;
-      document.getElementById('calledHome').value = record.calledHome;
-      document.getElementById('date').value = formatDate(record.date);
-      document.getElementById('trigger').value = record.trigger;
-
-      // Show the edit modal
-      const editModal = new bootstrap.Modal(document.getElementById('editModal'));
-      editModal.show();
-
-      // Save button event listener
-      document.getElementById('saveButton').addEventListener('click', () => {
-        const updatedRecord = {
-          location: document.getElementById('location').value,
-          severity: document.getElementById('severity').value,
-          duration: document.getElementById('duration').value,
-          vomiting: document.getElementById('vomiting').value,
-          calledHome: document.getElementById('calledHome').value,
-          date: new Date(document.getElementById('date').value),
-          trigger: document.getElementById('trigger').value,
-        };
-
-        // Send the updated record to the API for saving
-        fetch(`/api/data/${recordId}`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(updatedRecord),
-        })
-          .then(() => {
-            // Refresh the table after saving the updated record
-            fetchTableData();
-            // Close the edit modal
-            editModal.hide();
-          })
-          .catch((error) => {
-            console.log('Error updating record:', error);
-          });
-      });
-    })
-    .catch((error) => {
-      console.log('Error fetching record:', error);
-    });
+// dbData.js
+function unlockPage() {
+  const modalBackdrop = document.querySelector('.modal-backdrop');
+  if (modalBackdrop) {
+    modalBackdrop.remove();
+  }
 }
 
-// Function to format date as DD-MM-YYYY
-function formatDate(dateString) {
-  const date = new Date(dateString);
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
-  return `${day}-${month}-${year}`;
-}
-
-// Fetch table data
 function fetchTableData() {
   fetch('/api/data')
     .then((response) => response.json())
@@ -169,23 +111,78 @@ function fetchTableData() {
     })
     .finally(() => {
       // Unlock the screen after updating or deleting a record
-      const modalBackdrop = document.querySelector('.modal-backdrop');
-      if (modalBackdrop) {
-        modalBackdrop.remove();
-      }
+      unlockPage();
     });
 }
 
-// Function to handle deleting a record
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  return `${day}-${month}-${year}`;
+}
+
+function editRecord(recordId) {
+  fetch(`/api/data/${recordId}`)
+    .then((response) => response.json())
+    .then((record) => {
+      document.getElementById('editModalLabel').textContent = 'Edit Record';
+      document.getElementById('location').value = record.location;
+      document.getElementById('severity').value = record.severity;
+      document.getElementById('duration').value = record.duration;
+      document.getElementById('vomiting').value = record.vomiting;
+      document.getElementById('calledHome').value = record.calledHome;
+      document.getElementById('date').valueAsDate = new Date(record.date); // Set the date value
+      document.getElementById('trigger').value = record.trigger;
+      document.getElementById('note').value = record.note;
+
+      const editModal = new bootstrap.Modal(document.getElementById('editModal'));
+      editModal.show();
+
+      const saveButton = document.getElementById('saveButton');
+      saveButton.addEventListener('click', () => {
+        const updatedRecord = {
+          location: document.getElementById('location').value,
+          severity: document.getElementById('severity').value,
+          duration: document.getElementById('duration').value,
+          vomiting: document.getElementById('vomiting').value,
+          calledHome: document.getElementById('calledHome').value,
+          date: new Date(document.getElementById('date').value),
+          trigger: document.getElementById('trigger').value,
+          note: document.getElementById('note').value,
+        };
+
+        fetch(`/api/data/${recordId}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(updatedRecord),
+        })
+          .then(() => {
+            fetchTableData();
+            editModal.hide();
+            unlockPage();
+          })
+          .catch((error) => {
+            console.log('Error updating record:', error);
+          });
+      });
+    })
+    .catch((error) => {
+      console.log('Error fetching record:', error);
+    });
+}
+
 function deleteRecord(recordId) {
-  // Confirm deletion with the user
-  if (confirm('Are you sure you want to delete this record?')) {
-    // Send delete request to the API
+  const confirmation = confirm('Are you sure you want to delete this record?');
+
+  if (confirmation) {
     fetch(`/api/data/${recordId}`, {
       method: 'DELETE',
     })
       .then(() => {
-        // Refresh the table after deleting the record
         fetchTableData();
       })
       .catch((error) => {
@@ -194,5 +191,19 @@ function deleteRecord(recordId) {
   }
 }
 
+function fetchTotalRecordCount() {
+  fetch('/api/data')
+    .then((response) => response.json())
+    .then((data) => {
+      const totalRecordsElement = document.getElementById('totalRecords');
+      totalRecordsElement.textContent = `(${data.length})`;
+    })
+    .catch((error) => {
+      console.log('Error fetching total record count:', error);
+    });
+}
+
 // Fetch initial table data on page load
 fetchTableData();
+// Fetch total record count
+fetchTotalRecordCount();
